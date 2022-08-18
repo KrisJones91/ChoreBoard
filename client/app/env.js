e7a894af9f97289c5492aecb6a07253f745e0b62
Kris Jones <kristop.jones@gmail.com>
2022-08-18 14:38:02 -0600

    first commit

---
+++ client/app/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-1s3xbyye.us.auth0.com'
+export const audience = 'https://choreboard'
+export const clientId = 'DHWjhc48yhUpiPm0BZ4QX6AyBWxZyEcO'
